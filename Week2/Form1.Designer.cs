29a561d5a764b8fb3c860ad3a223d71923fcf47a
Nathan3122000 <nagusalim@student.ciputra.ac.id>
2022-03-14 09:29:32 +0700

    Add project files.

---
+++ Week2/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿
+namespace Week2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.button = new System.Windows.Forms.Button();
+            this.label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(12, 34);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(355, 22);
+            this.textBox.TabIndex = 0;
+            // 
+            // button
+            // 
+            this.button.Location = new System.Drawing.Point(12, 76);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(75, 23);
+            this.button.TabIndex = 1;
+            this.button.Text = "Proses";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(12, 117);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(63, 17);
+            this.label.TabIndex = 2;
+            this.label.Text = "[EMPTY]";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.button);
+            this.Controls.Add(this.textBox);
+            this.Name = "Form1";
+            this.Text = "Form Pengaturan Warna";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.Label label;
+    }
+}
+
---
+++ Week2/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Week2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            if (textBox.Text.Substring(0, 4) == "ISI:")
+                label.Text = textBox.Text.Substring(4);
+            else if (textBox.Text == "BESARKAN")
+                label.Text = label.Text.ToUpper();
+            else if (textBox.Text == "KECILKAN")
+                label.Text = label.Text.ToLower();
+            else if (textBox.Text == "SHOWN")
+                label.Show();
+            else if (textBox.Text == "HIDE")
+                label.Hide();
+            else if (textBox.Text.Substring(0, 6) == "WARNA:")
+            {
+                if (textBox.Text.Substring(6) == "MERAH")
+                    label.ForeColor = Color.Red;
+                else if (textBox.Text.Substring(6) == "BIRU")
+                    label.ForeColor = Color.Blue;
+                else if (textBox.Text.Substring(6) == "HIJAU")
+                    label.ForeColor = Color.Green;
+                else if (textBox.Text.Substring(6) == "DEFAULT")
+                    label.ForeColor = Color.Black;
+            }
+            else if (textBox.Text == "RESTART")
+                label.Text = "[EMPTY]";
+            else
+                MessageBox.Show("Syntax tidak terdaftar!", "ERROR");
+        }
+    }
+}
